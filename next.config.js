45e64d59b4c66b422bf9df168e09cc8ed501be7d
Pritam Kumar <pritam.webdesk@gmail.com>
2025-02-16 23:18:01 +0530

    update

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    forceDynamic: true, // Force all pages to be dynamically rendered
+  },
+  // eslint: {
+  //   ignoreDuringBuilds: true, // Ignore ESLint errors during the build
+  // },
+  // typescript: {
+  //   ignoreBuildErrors: true, // Ignore TypeScript errors during the build
+  // },
+};
+
+export default nextConfig;
